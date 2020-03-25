a26f4969a48eb89998356383b5daead75478f4f6
Luciano Barcellos <lcbarcellos@gmail.com>
2020-03-25 12:24:59 -0300

    Automatize new version generation

--- scripts/release.sh
+++ scripts/release.sh
@@ -66,12 +66,75 @@ run_command() {
     local CMD="$1"
     shift
     echo === "$@" =======================
-    "$CMD" && echo --- OK /"$@" ------------------- ||
-              echo --- ERROR /"$@" ----------------
+    "$CMD" && echo --- OK /"$@" -------------------
     echo
 }
 
-run_command create_tag "Creating tag"
-run_command create_release "Creating release"
-run_command upload_file "Uploading plugin file"
-run_command push_update "Uploading update information"
+from_src() {
+    [ "${basedir}" == "" ]
+}
+
+generate_new_commit() {
+    git checkout release
+    git merge --no-commit master
+    sed --in-place -e '
+        /<version>/ {
+            s_\(\s*<version\s*>\)\s*\(.*[^0-9]\)\([0-9]\+\)\s*\(</version>.*\)_@#@\1@#@\2@#@\3@#@\4@#@_
+            h
+            s/@#@.*@#@.*@#@\(.*\)@#@.*@#@/\1_/
+        :next
+            s/9_/_0/
+            t next
+            s/8_/9/
+            s/7_/8/
+            s/6_/7/
+            s/5_/6/
+            s/4_/5/
+            s/3_/4/
+            s/2_/3/
+            s/1_/2/
+            s/0_/1/
+            s/^_/1/
+            s/.*/@#@\0@#@/
+            G
+            s/@#@\(.*\)@#@.*@#@\(.*\)@#@\(.*\)@#@.*@#@\(.*\)@#@/\2\3\1\4/
+            b printAll
+        }
+        b
+        :printAll
+        n
+        b printAll
+    ' pom.xml
+    git add pom.xml
+    VERSION=$(sed -n -e '
+        /<version>/ {
+            s_\(\s*<version\s*>\)\s*\(.*[^0-9]\)\([0-9]\+\)\s*\(</version>.*\)_\2\3_
+            p
+            q
+        }
+    ' 'pom.xml')
+    git commit -m "Version ${VERSION}"
+    mvn clean package
+}
+
+push_branches() {
+    git push origin master release
+}
+
+halt_on_error() {
+    set -e
+}
+
+
+halt_on_error
+
+if from_src
+then
+    generate_new_commit
+    push_branches
+else
+    run_command create_tag "Creating tag"
+    run_command create_release "Creating release"
+    run_command upload_file "Uploading plugin file"
+    run_command push_update "Uploading update information"
+fi
