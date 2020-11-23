1a542fbe3b090fed9e9be81f10f4582e75d69905
Luciano Barcellos <lcbarcellos@gmail.com>
2020-11-23 04:31:50 -0300

    Utility functions for templating

--- src/main/java/br/ufes/inf/nemo/ufo/protege/Util.java
+++ src/main/java/br/ufes/inf/nemo/ufo/protege/Util.java
@@ -12,6 +12,9 @@ import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 /**
@@ -110,6 +113,82 @@ public abstract class Util {
                 clazz.getResourceAsStream(resourceName), "UTF-8")
         ) {
             return scanner.useDelimiter("\\A").next();
+        }
+    }
+
+    /**
+     * Create a new string scanner for a regular expression.
+     *
+     * @param regexString The regular expression to be used by scanner
+     * @return The scanner
+     */
+    public static StringScanner stringScanner(String regexString) {
+        return new StringScanner(regexString);
+    }
+
+    /**
+     * Utility class to scan a string, spliting it according to a regular
+     * expression.
+     * <br>
+     */
+    public static class StringScanner {
+
+        private final Pattern pattern;
+
+        StringScanner(String regexString) {
+            pattern = Pattern.compile(regexString);
+        }
+
+        /**
+         * Scans given string and reporting matched and unmatched blocks.
+         * <br>
+         * A matcher is created based on regular expression of this scanner for
+         * the given string. The string is then scanned by occurrence of the
+         * regular expression, and the consumers are invoked to accept
+         * sequentially the substrings matching and not matching the regular
+         * expression.
+         *
+         * @param str The string to be scanned
+         * @param unmatchConsumer The consumer of unmatched blocks
+         * @param matcherConsumer The consumer of matched blocks
+         */
+        public void scan(String str,
+                Consumer<String> unmatchConsumer,
+                Consumer<Matcher> matcherConsumer) {
+
+            Matcher matcher = pattern.matcher(str);
+            int startIndex = 0;
+            while (matcher.find()) {
+                int matcherStart = matcher.start();
+                if (startIndex < matcherStart) {
+                    unmatchConsumer.accept(
+                            str.substring(startIndex, matcherStart));
+                }
+                matcherConsumer.accept(matcher);
+                startIndex = matcher.end();
+            }
+            if (startIndex < str.length()) {
+                unmatchConsumer.accept(str.substring(startIndex));
+            }
+        }
+
+        /**
+         * Replace expression matches by values returned by given function.
+         * <br>
+         * The string is scanned by occurrence of the regular expression and
+         * the given function is applied for every match. The result string has
+         * all matches replaced by the result of applying the function.
+         *
+         * @param str The string to be scanned
+         * @param doReplace Function replacing the matches
+         * @return Text with occurrences of regular expression replaced
+         */
+        public String replace(String str, Function<Matcher, String> doReplace) {
+            StringBuilder result = new StringBuilder();
+            scan(str, result::append, matcher -> {
+                result.append(doReplace.apply(matcher));
+            });
+            return result.toString();
         }
     }
 }
